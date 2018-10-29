7ff701cc5b0d2aa074cd67316a1253503b0df55d
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2018-10-29 01:15:49 +0100

    create new project

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SFE_BACK
 
 Plateforme ouverte à l&#39;enseignement à distance
+
+# Instructions
+
+1 - open new cmd  window in the workspace of the spring Tool
+
+2 - git clone https://ZaynabHAMILA@bitbucket.org/ZaynabHAMILA/sfe_back.git
---
+++ src/main/java/com/app/boot/SfeBackApplication.java
@@ -0,0 +1,12 @@
+package com.app.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfeBackApplication.class, args);
+	}
+}
