2e4452fa7a5ffc889d42c78d7d81687caae9fd78
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-03-27 17:50:37 +0100

    LevelDTO Class added

---
+++ src/main/java/com/app/boot/dto/LevelDTO.java
@@ -0,0 +1,34 @@
+package com.app.boot.dto;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.app.boot.model.Establishment;
+import com.app.boot.model.Field;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "${swagger.level-dto}")
+public class LevelDTO {
+
+	@ApiModelProperty(value = "${swagger.level-dto.id}")
+	private Long id;
+
+	@ApiModelProperty(value = "${swagger.level-dto.levelName}")
+	private String levelName;
+	
+	@ApiModelProperty(value = "${swagger.level-dto.description}")
+	private String description;
+	
+	@ApiModelProperty(value = "${swagger.level-dto.establishment}")
+	private Establishment establishment;
+
+	@ApiModelProperty(value = "${swagger.level-dto.fields}")
+	private Set<Field> fields;
+
+}
