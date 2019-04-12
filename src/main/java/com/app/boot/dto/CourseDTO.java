2bf8809d401a85012129766223990d7c782a2007
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-04-12 23:31:32 +0100

    CourseDTO added

---
+++ src/main/java/com/app/boot/dto/CourseDTO.java
@@ -0,0 +1,49 @@
+package com.app.boot.dto;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.app.boot.model.Class;
+import com.app.boot.model.Establishment;
+import com.app.boot.model.Field;
+import com.app.boot.model.Professor;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "${swagger.course-dto}")
+public class CourseDTO {
+
+	@ApiModelProperty(value = "${swagger.course-dto.id}")
+
+	private Long id;
+	@ApiModelProperty(value = "${swagger.course-dto.courseName}")
+
+	private String courseName;
+	@ApiModelProperty(value = "${swagger.course-dto.description}")
+
+	private String description;
+
+	@ApiModelProperty(value = "${swagger.course-dto.classe}")
+
+	private Class classe;
+
+	@ApiModelProperty(value = "${swagger.course-dto.professors}")
+
+	private Set<Professor> professors;
+
+}
